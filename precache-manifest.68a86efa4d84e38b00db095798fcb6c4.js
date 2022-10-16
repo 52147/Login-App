a36bffe53b5b6d6cab8658d6c862b2cb5dd76fc7
Your Name <debra4117@gmail.com>
2022-10-16 02:39:58 -0500

    Updates

---
+++ precache-manifest.68a86efa4d84e38b00db095798fcb6c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46895be58b3085c582885fef5a8af8e",
+    "url": "/Login-App/index.html"
+  },
+  {
+    "revision": "6fd4acbf8e0423ab56eb",
+    "url": "/Login-App/static/js/2.3f5fe382.chunk.js"
+  },
+  {
+    "revision": "515d69c448c84ebb1cca",
+    "url": "/Login-App/static/js/main.b6019195.chunk.js"
+  },
+  {
+    "revision": "22d7b7f105584729e513",
+    "url": "/Login-App/static/js/runtime-main.9932df90.js"
+  }
+]);
